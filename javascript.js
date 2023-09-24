92f83bdc0641a4db69dfd9f3e0017252163f614c
IntPDFX-PINTUCO <127631195+IntPDFX-PINTUCO@users.noreply.github.com>
2023-09-24 15:48:00 -0500

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,54 +1,57 @@
-tablero=document.getElementById("tablero")
-colores=tablero.getContext("2d")
-colores.strokeStyle="Black"
-colores.lineWidth=3
-colores.beginPath()
-colores.strokeStyle="Blue"
-colores.arc(200,200,40,0,2*Math.PI)
-colores.stroke()
-colores.beginPath()
-colores.strokeStyle="Yellow"
-colores.arc(243,255,40,0,2*Math.PI)
-colores.stroke()
-colores.beginPath()
-colores.strokeStyle="Black"
-colores.arc(285,200,40,0,2*Math.PI)
-colores.stroke()
-colores.beginPath()
-colores.strokeStyle="Green"
-colores.arc(330,255,40,0,2*Math.PI)
-colores.stroke()
-colores.beginPath()
-colores.strokeStyle="orangered"
-colores.arc(370,200,40,0,2*Math.PI)
-colores.stroke()
-var xi=0
-var yi=0
-var xf=0
-var yf=0
-var dibujando=false
-tablero.addEventListener("mousedown",micli) 
-tablero.addEventListener("mousemove",mimov) 
-tablero.addEventListener("mouseup",miu) 
-function micli(info){
-    xi=info.clientX-tablero.offsetLeft
-    yi=info.clientY-tablero.offsetTop
-dibujando=true
+tablero = document.getElementById("tablero")
+colores = tablero.getContext("2d")
+colores.strokeStyle = "black"
+colores.lineWidth = 2.5
+var xi = 0
+var yi = 0
+var xf = 0
+var yf = 0
+var dibujando = false
+tablero.addEventListener("mousedown", micli)
+tablero.addEventListener("mousemove", mimov)
+tablero.addEventListener("mouseup", miu)
+tablero.addEventListener("touchstart", touin)
+tablero.addEventListener("touchmove", toumov)
+function micli(info) {
+    xf = info.clientX - tablero.offsetLeft
+    yf = info.clientY - tablero.offsetTop
+    dibujando = true
 }
-function mimov(info){
-    xi=info.clientX-tablero.offsetLeft
-    yi=info.clientY-tablero.offsetTop+8
-    if(dibujando==true){
+function mimov(info) {
+    xi = info.clientX - tablero.offsetLeft
+    yi = info.clientY - tablero.offsetTop + 0.5
+    if (dibujando == true) {
 
         colores.beginPath()
-        colores.strokeStyle="orangered"
-        colores.moveTo(xi,yi)
-        colores.lineTo(xf,yf)
+        colores.strokeStyle = document.getElementById("color").value
+        colores.moveTo(xi, yi)
+        colores.lineTo(xf, yf)
         colores.stroke()
     }
-    xf=xi
-    yf=yi
+    xf = xi
+    yf = yi
 }
-function miu(info){
-dibujando=false
+function miu(info) {
+    dibujando = false
+}
+function touin(info) {
+    xf = info.touches[0].clientX - tablero.offsetLeft
+    yf = info.touches[0].clientY - tablero.offsetTop
+}
+function toumov(info) {
+    xi = info.touches[0].clientX - tablero.offsetLeft
+    yi = info.touches[0].clientY - tablero.offsetTop + 0.5
+
+    colores.beginPath()
+    colores.strokeStyle = document.getElementById("color").value
+    colores.moveTo(xi, yi)
+    colores.lineTo(xf, yf)
+    colores.stroke()
+    xf = xi
+    yf = yi
+}
+if (screen.width < 992) {
+    tablero.width = screen.width - 70
+    tablero.height = screen.height - 200
+    document.body.style.overflow = "hidden"
 }
